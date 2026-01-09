6d6ea21715ae12277a3d1ea324737308545321ca
brkybyr <berkayboyarr@gmail.com>
2026-01-09 09:01:20 +0100

    added swagger and example users

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -15,8 +15,39 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+# Router oluştur
+router = routers.DefaultRouter()
+
+from accounts.views import UserViewSet
+# from tasks.views import TaskViewSet
+# from courses.views import CourseViewSet
+
+router.register(r'users', UserViewSet)
+# router.register(r'tasks', TaskViewSet)
+# router.register(r'courses', CourseViewSet)
+
+# Swagger schema
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Taskify API",
+      default_version='v1',
+      description="Taskify study & task management API",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    
+    # Swagger UI
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='redoc'),
 ]
